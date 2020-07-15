3afd5f826e7c644f54db954735deb61ce4261d29
Augusto Telles <40339166+toguvr@users.noreply.github.com>
2020-07-15 13:02:08 -0300

    subindo

---
+++ goBarberApp/src/pages/SignIn/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Image } from 'react-native';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import logoImg from '../../assets/logo.png';
+import { Container, Title } from './styles';
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+      <Title>Faça seu login</Title>
+      <Input name="email" icon="mail" placeholder="Email" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button>Entrar</Button>
+    </Container>
+  );
+};
+
+export default SignIn;
